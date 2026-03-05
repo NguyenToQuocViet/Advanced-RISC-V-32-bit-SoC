64a5b25ce0b6ebfbc1768de725e2e603b1a96f7e
NguyenToQuocViet <viet.nguyenluminous@hcmut.edu.vn>
2026-03-05 22:25:54 +0700

    feat(icache): implement address decoder, storage array, comparator and refill buffer

---
+++ rtl/icache.sv
@@ -0,0 +1,79 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2026 NGUYEN TO QUOC VIET
+// Ho Chi Minh City University of Technology (HCMUT-VNU)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// -----------------------------------------------------------------------------
+// Project      : Advanced RISC-V 32-bit Processor
+// Module       : icache
+// Description  : 4KB Direct-Mapped Instruction Cache
+//
+// Author       : NGUYEN TO QUOC VIET
+// Date         : 2026-03-04
+// Version      : 1.0
+// -----------------------------------------------------------------------------
+
+module icache 
+    import cache_pkg::*;
+(
+    //system
+    input logic clk, rst_n,
+
+    //IF - i-cache interface
+    input logic [ADDR_WIDTH-1:0] pc,
+    input logic if_req,
+
+    output logic [DATA_WIDTH-1:0] instr,
+    output logic icache_ready,
+    output logic icache_valid,
+
+    //arbiter - i-cache interface
+    input logic [DATA_WIDTH-1:0] arb_rdata,
+    input logic arb_valid,
+    input logic arb_last,
+    input logic arb_grant,
+
+    output logic icache_req,
+    output logic [ADDR_WIDTH-1:0] icache_addr
+);
+    //address decode
+    logic [WORD_SEL_BITS-1:0]   pc_word_sel;
+    logic [IC_IDX_BITS-1:0]     pc_idx;
+    logic [IC_TAG_BITS-1:0]     pc_tag;
+
+    assign pc_word_sel  = pc[WORD_OFF_BITS +: WORD_SEL_BITS];   //3 - 2
+    assign pc_idx       = pc[LINE_OFF_BITS +: IC_IDX_BITS];     //11 - 4
+    assign pc_tag       = pc[ADDR_WIDTH-1 -: IC_TAG_BITS];      //31 - 12 
+
+    //storage
+    logic [IC_TAG_BITS-1:0] cache_tag [IC_SETS];
+    logic [DATA_WIDTH-1:0] cache_data [IC_SETS][WORDS_PER_LINE];
+    logic [IC_SETS-1:0] cache_valid;    //for simple resest, not large
+
+    //comparator
+    logic cache_hit;
+    logic [DATA_WIDTH-1:0] hit_data;
+
+    assign cache_hit = cache_valid[pc_idx] && (cache_tag[pc_idx] == pc_tag);
+    assign hit_data  = cache_data[pc_idx][pc_word_sel];
+
+    //refill buffer
+    logic [DATA_WIDTH-1:0]      rf_buffer [WORDS_PER_LINE];
+    logic [WORDS_PER_LINE-1:0]  rf_valid;
+    logic [IC_TAG_BITS-1:0]     rf_tag;
+    logic [IC_IDX_BITS-1:0]     rf_idx;
+    logic [WORD_SEL_BITS-1:0]   rf_word_sel;
+
+    logic rf_buffer_hit;
+    assign rf_buffer_hit = rf_valid[pc_word_sel] && (rf_idx == pc_idx) && (rf_tag == pc_tag);
+
+    //fsm
+endmodule
